313e851844ee7976677fc97873e458cbbb286615
root <root@ip-10-42-39-200.ec2.internal>
2025-11-07 13:32:40 +0000

    Fix code style: remove leading spaces from execute.c

--- execute.c
+++ execute.c
@@ -1,39 +1,39 @@
 #include "shell.h"
 
 /**
- * execute - executes command
- * @args: array of command arguments
- * Return: 1 for success
- */
+* execute - executes command
+* @args: array of command arguments
+* Return: 1 for success
+*/
 int execute(char **args)
 {
-    pid_t pid;
-    int status;
-    char *command_path = find_path(args[0]);
+pid_t pid;
+int status;
+char *command_path = find_path(args[0]);
 
-    if (command_path == NULL)
-    {
-        fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-        return (1);
-    }
+if (command_path == NULL)
+{
+fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+return (1);
+}
 
-    pid = fork();
-    if (pid == 0)
-    {
-        if (execve(command_path, args, environ) == -1)
-            perror("execve");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid < 0)
-    {
-        perror("fork");
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-    }
+pid = fork();
+if (pid == 0)
+{
+if (execve(command_path, args, environ) == -1)
+perror("execve");
+exit(EXIT_FAILURE);
+}
+else if (pid < 0)
+{
+perror("fork");
+}
+else
+{
+waitpid(pid, &status, 0);
+}
 
-    free(command_path);
-    return (1);
+free(command_path);
+return (1);
 }
 
